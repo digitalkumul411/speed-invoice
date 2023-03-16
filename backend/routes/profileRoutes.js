ce2b1465c154ebac89842bd451b9c795f4418341
JANGAP411 <angapj@gmail.com>
2023-03-16 15:01:46 +1000

    Revert "Added basic profile routes for the endpoints"
    This reverts commit 013ba3351e01f98b048b5c0dfc01ec72166bf583.

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
-const { getProfiles, getProfile, updateProfile, removeProfile, createProfile } = require("../controllers/profile");
+const { getProfiles } = require("../controllers/profile");
 
-router.route("/profile").get(getProfiles).post(createProfile);
-router.route('/profile/:id').get(getProfile).patch(updateProfile).delete(removeProfile)
+router.route("/profile").get(getProfiles);
 
 module.exports = router;
