9c2a0e39eed04fc0f0c28a5db997f9062e9ae19c
JANGAP411 <angapj@gmail.com>
2023-03-16 15:44:10 +1000

    Added profile routes

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
-const { getProfiles } = require("../controllers/profile");
 
-router.route("/profile").get(getProfiles);
+const {
+  getProfiles,
+  getProfile,
+  updateProfile,
+  removeProfile,
+  createProfile,
+} = require("../controllers/profile");
+
+router.route("/profile").get(getProfiles).post(createProfile);
+router
+  .route("/profile/:id")
+  .get(getProfile)
+  .patch(updateProfile)
+  .delete(removeProfile);
 
 module.exports = router;
