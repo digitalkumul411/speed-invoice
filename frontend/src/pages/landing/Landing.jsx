13ce73f98346eb95b8ad5cc7cdde3bc49c6b58b0
JANGAP411 <angapj@gmail.com>
2023-03-16 15:29:49 +1000

    Redo the frontend creation using create-react-app command

---
+++ frontend/src/pages/landing/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+const Landing = () => {
+  return (
+    <div>
+      <Header />
+      Landing
+      <Footer />
+    </div>
+  );
+};
+
+export default Landing;
